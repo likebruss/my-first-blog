16d9f497737443e3e1b14a2c714e59567e395d3e
Krzysztof Wołowski <krzysztof@mojebudowy.pl>
2017-05-07 16:15:04 +0200

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'i&gulc-@_&zdk5jm(hq^plictvmu*7=!3@#olvv9+8=v55la8c'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
